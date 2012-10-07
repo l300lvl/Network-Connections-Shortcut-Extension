32f20878fe59dfa4857be4c9321be47704d916a1
l300lvl <atz3@yahoo.ca>
2012-10-07 02:54:45 -0500

    fix 3.6 compat

--- network-connections-shortcut@l300lvl.co.nr/extension.js
+++ network-connections-shortcut@l300lvl.co.nr/extension.js
@@ -1,5 +1,6 @@
 
 const St = imports.gi.St;
+const Config = imports.misc.config;
 const Main = imports.ui.main;
 const Lang = imports.lang;
 const Shell = imports.gi.Shell;
@@ -11,10 +12,6 @@ function _onPowerStatsActivate() {
     Main.overview.hide();
     let app = Shell.AppSystem.get_default().lookup_app('nm-connection-editor.desktop');
     app.activate();
-}
-
-function init() {
-    menu = Main.panel._statusArea.network.menu;
 }
 
 function enable() {
@@ -29,4 +26,25 @@ function disable() {
     if (item) {
         item.destroy();
     }
+}
+
+let age;
+
+function init() {
+    let current_version = Config.PACKAGE_VERSION.split('.');
+    if (current_version.length != 3 || current_version[0] != 3) throw new Error("Strange version number (extension.js:35).");
+    
+    switch (current_version[1]) {
+        case"2": global.log("Warning of extension [" + metadata.uuid + "]:\n              Old development release detected (" + Config.PACKAGE_VERSION + "). You should upgrade!\n");   //eak
+        case"3":  ;
+        case"4": age = "old";
+            break;
+        case"5": global.log("Warning of extension [" + metadata.uuid + "]:\n              Development release detected (" + Config.PACKAGE_VERSION + "). Loading as a 3.6 release.\n"); //eak
+        case"6": age = "new";
+            break;
+        default: throw new Error("Strange version number (extension.js:45).");
+    }
+
+    if (age=="old") menu = Main.panel._statusArea.network.menu;
+    else            menu = Main.panel.statusArea.network.menu;
 }
